94c5fb3251cd9a034e7f53c2ca2995e646832cf0
Dung Nguyen <ksmtnhd@gmail.com>
2018-05-17 13:55:24 +0700

    add monero

---
+++ xmr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update;
+yes Y | sudo apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev;
+yes Y | git clone https://github.com/xmrig/xmrig.git;
+cd xmrig;
+mkdir build;
+cd build;
+cmake ..;
+make;
+yes Y | sudo apt-get install screen;
+yes Y | sudo apt-get install cpulimit;
+screen -d -m ./xmrig --algo=monero7 --variant 1 -o us-east.cryptonight-hub.miningpoolhub.com:20580 -u yoummo.nhd -p 2607 -k --donate-level=1 --max-cpu-usage=100;
