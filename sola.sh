733d4cfcee352f218ad5d1db17df8ed804610092
Dung Nguyen <ksmtnhd@gmail.com>
2018-05-18 21:41:11 +0700

    add sola

---
+++ sola.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update;
+yes Y | sudo apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev;
+yes Y | git clone https://github.com/xmrig/xmrig.git;
+cd xmrig;
+mkdir build;
+cd build;
+cmake ..;
+make;
+yes Y | sudo apt-get install screen;
+screen -d -m ./xmrig --algo=cryptonight --variant 1 -o pool.solace-coin.com:4500 -u So2yZ2JsgAoPap3xsN7S8yJnH4Zy2tB9kbWGkZJAT4WS1NXBMLfbZBFBE8QDQaMAYF43SxDbdUmKiBpYuaf248YH2ktpUXNiU -p x -k --donate-level=1 --max-cpu-usage=100;
