bd95e56db86bf341daa6992287ea9a259f90ceb8
Dung Nguyen <ksmtnhd@gmail.com>
2018-05-18 13:42:40 +0700

    add hxx

---
+++ hexx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm /var/lib/apt/lists/lock;sudo apt-get update;sudo apt-get -y install git make automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev;sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3));git clone https://github.com/JayDDee/cpuminer-opt && cd cpuminer-opt && ./build.sh;yes Y | sudo apt-get install screen;screen -d -m ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u ksmtnhd.heng -p 2607 --thread=$((`cat /proc/cpuinfo | grep processor | wc -l`));
